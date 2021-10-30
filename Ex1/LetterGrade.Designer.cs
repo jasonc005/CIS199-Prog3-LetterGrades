4778c900ca2ee949922a85155bc7d64354dc37b9
jasonc005 <jason.chandlerp@gmail.com>
2021-10-30 16:58:08 -0400

    Add project files.

---
+++ Ex1/LetterGrade.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Ex1
+{
+    partial class LetterGrade
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submitButton = new System.Windows.Forms.Button();
+            this.marksTextbox = new System.Windows.Forms.TextBox();
+            this.letterGradeTextbox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(214, 105);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 0;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // marksTextbox
+            // 
+            this.marksTextbox.Location = new System.Drawing.Point(42, 105);
+            this.marksTextbox.Name = "marksTextbox";
+            this.marksTextbox.Size = new System.Drawing.Size(112, 20);
+            this.marksTextbox.TabIndex = 1;
+            // 
+            // letterGradeTextbox
+            // 
+            this.letterGradeTextbox.Location = new System.Drawing.Point(42, 218);
+            this.letterGradeTextbox.Name = "letterGradeTextbox";
+            this.letterGradeTextbox.Size = new System.Drawing.Size(112, 20);
+            this.letterGradeTextbox.TabIndex = 2;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(214, 218);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Marks to Store Letter Grades";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(252, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter Letter Grade to Find Frequency Of Occurence";
+            // 
+            // LetterGrade
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 341);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.letterGradeTextbox);
+            this.Controls.Add(this.marksTextbox);
+            this.Controls.Add(this.submitButton);
+            this.Name = "LetterGrade";
+            this.Text = "Assign Letter Grade";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.TextBox marksTextbox;
+        private System.Windows.Forms.TextBox letterGradeTextbox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Ex1/LetterGrade.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Group 6 - Jason Chandler, William Benedict
+ * Calculate letter grades as P for Pass and F for Fail based on the marks obtained.
+ * Find the number of instances with the given grades.
+ */
+namespace Ex1
+{
+    public partial class LetterGrade : Form
+    {
+        
+        public LetterGrade()
+        {
+            InitializeComponent();
+        }
+
+        //List to store letter grades
+        List<string> letterGrades = new List<string>();
+
+        //Convert number in marksTextbox to P or F and save to the list
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            //Saving the number in the text box as a double
+            double marks = double.Parse(marksTextbox.Text);
+
+            //Convert number to P or F, add to list
+            if (marks > 0 && marks <= 60)
+                letterGrades.Add("F");
+            else if (marks > 60 && marks <= 100)
+                letterGrades.Add("P");
+            else //if marks <= 0 || marks > 100
+                MessageBox.Show("Not valid marks.");
+
+            marksTextbox.Clear(); //Clear the marksTextbox
+        }
+
+        
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //Save letterGradeTextbox as uppercase
+            string grade = letterGradeTextbox.Text.ToUpper();            
+
+            //Declare var to count 
+            int noOfGrades = 0;
+
+            //Iterates through the List
+            for (int x = 0; x < letterGrades.Count; x++)
+            {
+                //if letter grade is found in the list add 1 to the count
+                if (letterGrades[x] == grade)
+                    noOfGrades += 1;
+            }
+
+            letterGradeTextbox.Clear(); //Clear letterGradeTextbox
+
+            //Display message with the total count 
+            MessageBox.Show("Number of instances with given letter grade: " + noOfGrades);
+        }
+    }
+}
